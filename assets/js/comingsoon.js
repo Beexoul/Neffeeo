ca07ca1dffa5e5ebf993534f34f869b8a5a4ae9f
Shiva Raj Paudel <official.beexoul@gmail.com>
2024-04-25 12:09:11 +0545

    Update comingsoon.js

--- assets/js/comingsoon.js
+++ assets/js/comingsoon.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("April 21, 2024 12:00:00").getTime();
+    const countDate = new Date("May 1, 2024 12:00:00").getTime();
     const currentTime = new Date().getTime();
     console.log(currentTime);
     const gap = countDate - currentTime;
